71b4e6f69b8896b9597d186730875f9b48cc9e76
Kyle <44553820+KyleRokuKyu@users.noreply.github.com>
2023-05-23 23:47:40 -0400

    Created mod SingleCity.  There was a request for a mod or feature that limits the game to only 1 city in the whole world.  This does that.  It's quick and dirty but gets the job done just fine

---
+++ SingleCity/SingleCity/SingleCityMod.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using VoxelTycoon.Modding;
+using HarmonyLib;
+using VoxelTycoon.Cities;
+
+public class SingleCityMod : Mod
+{
+	private Harmony _harmony;
+
+	protected override void Initialize()
+	{
+		Harmony.DEBUG = false;
+		_harmony = new Harmony("com.kylerokukyu.singlecity");
+		FileLog.Reset();
+		_harmony.PatchAll();
+		Debug.Log("SingleCity is Initialized!");
+	}
+
+	protected override void Deinitialize()
+	{
+		_harmony.UnpatchAll("com.kylerokukyu.singlecity");
+		_harmony = null;
+		Debug.Log("SingleCity is Deinitialized!");
+	}
+}
+
+[HarmonyPatch(typeof(City), "Initialize")]
+class Patch
+{
+	public static bool first = true;
+
+	static bool Prefix(ref City __instance)
+	{
+		if (__instance.Region.Center.X == 200 && __instance.Region.Center.Z == 200 && first)
+		{
+			first = false;
+			return true;
+		}
+		return false;
+	}
+}
